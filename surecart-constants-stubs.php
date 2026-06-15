2a8d320e2673673e1977a1774150d41c6cf58abe
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-06-15 13:36:40 +0600

    Generate stubs for SureCart 2.0.1

--- surecart-constants-stubs.php
+++ surecart-constants-stubs.php
@@ -1,4 +1,40 @@
 <?php
+/**
+ * Generated stub declarations for SureCart.
+ * @see https://surecart.com
+ * @see https://github.com/mralaminahamed/phpstan-surecart-stubs
+ */
 
-// This file is generated. Do not edit it manually.
-// Run `composer generate` to regenerate.
+\define('SURECART_APP_CORE_DIR', __DIR__);
+\define('SURECART_APP_CORE_SRC_DIR', \SURECART_APP_CORE_DIR . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR);
+\define('SURECART_DIR', __DIR__);
+\define('SURECART_CONFIG_KEY', 'surecart.config');
+\define('SURECART_APPLICATION_KEY', 'surecart.application.application');
+\define('SURECART_APPLICATION_GENERIC_FACTORY_KEY', 'surecart.application.generic_factory');
+\define('SURECART_APPLICATION_CLOSURE_FACTORY_KEY', 'surecart.application.closure_factory');
+\define('SURECART_HELPERS_HANDLER_FACTORY_KEY', 'surecart.handlers.helper_factory');
+\define('SURECART_WORDPRESS_HTTP_KERNEL_KEY', 'surecart.kernels.wordpress_http_kernel');
+\define('SURECART_SESSION_KEY', 'surecart.session');
+\define('SURECART_REQUEST_KEY', 'surecart.request');
+\define('SURECART_RESPONSE_KEY', 'surecart.response');
+\define('SURECART_EXCEPTIONS_ERROR_HANDLER_KEY', 'surecart.exceptions.error_handler');
+\define('SURECART_EXCEPTIONS_CONFIGURATION_ERROR_HANDLER_KEY', 'surecart.exceptions.configuration_error_handler');
+\define('SURECART_RESPONSE_SERVICE_KEY', 'surecart.responses.response_service');
+\define('SURECART_ROUTING_ROUTER_KEY', 'surecart.routing.router');
+\define('SURECART_ROUTING_ROUTE_BLUEPRINT_KEY', 'surecart.routing.route_registrar');
+\define('SURECART_ROUTING_CONDITIONS_CONDITION_FACTORY_KEY', 'surecart.routing.conditions.condition_factory');
+\define('SURECART_ROUTING_CONDITION_TYPES_KEY', 'surecart.routing.conditions.condition_types');
+\define('SURECART_VIEW_SERVICE_KEY', 'surecart.view.view_service');
+\define('SURECART_VIEW_COMPOSE_ACTION_KEY', 'surecart.view.view_compose_action');
+\define('SURECART_VIEW_ENGINE_KEY', 'surecart.view.view_engine');
+\define('SURECART_VIEW_PHP_VIEW_ENGINE_KEY', 'surecart.view.php_view_engine');
+\define('SURECART_SERVICE_PROVIDERS_KEY', 'surecart.service_providers');
+\define('SURECART_FLASH_KEY', 'surecart.flash.flash');
+\define('SURECART_OLD_INPUT_KEY', 'surecart.old_input.old_input');
+\define('SURECART_CSRF_KEY', 'surecart.csrf.csrf');
+\define('SURECART_PLUGIN_FILE', __FILE__);
+\define('SURECART_PLUGIN_DIR_NAME', \dirname(\plugin_basename(\SURECART_PLUGIN_FILE)));
+\define('SURECART_LANGUAGE_DIR', __DIR__ . \DIRECTORY_SEPARATOR . 'languages');
+\define('SURECART_APP_URL', 'https://app.surecart.com');
+\define('SURECART_API_URL', 'https://api.surecart.com');
+\define('SURECART_CDN_IMAGE_BASE', 'https://surecart.com/cdn-cgi/image');
